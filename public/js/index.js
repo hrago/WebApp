bac086ce16a02e894e795ea7a383f7f555fec184
user <user@Macintosh-2.local>
2018-05-04 21:04:38 -0400

    add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -26,21 +26,15 @@ socket.on('newLocationMessage', function(message) {
   jQuery('#messages').append(li);
 });
 
-socket.emit('createMessage', {
-  from: 'Frank',
-  text: 'Hi'
-}, function (data) {
-  console.log('Got it', data);
-});
-
 jQuery('#message-form').on('submit', function (e) {
+  var messageTextbox = jQuery('[name=message]');
   e.preventDefault();
-
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-  })
+    messageTextbox.val('')
+  });
 });
 
 var locationButton = jQuery('#send-location');
@@ -48,12 +42,17 @@ locationButton.on('click', function () {
   if(!navigator.geolocation) {
     return alert('Geolocation not supported by your browser.');
   }
+
+  locationButton.attr('disabled', 'disabled').text('Sending location...')
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
           latitude: position.coords.latitude,
           longitude: position.coords.longitude
         });
     }, function () {
+      locationButton.removeAttr('disabled').text('Send location');
       alert('Unable to fetch location.');
     });
 });
